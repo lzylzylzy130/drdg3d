53ba2fcba196064a63204a45e08971f010b1a955
Lu Zeyu <luzeyu@mail.ustc.edu.cn>
2023-07-26 15:42:40 +0800

    Add output

--- examples/basin_alper/draw_ruptime.m
+++ examples/basin_alper/draw_ruptime.m
@@ -16,7 +16,7 @@ nx = gather_fault_var( data_dir, nproc, 'nx' );
 t = gather_fault_var( data_dir, nproc, 'ruptime' );
 fault_id = gather_fault_var1d( data_dir, nproc, 'fault_id' );
 
-idx = find(mean(nx(:,:))>0 & fault_id == 1);
+idx = find(mean(nx(:,:))>0 & fault_id == 2);
 x = x(:,idx);
 y = y(:,idx);
 z = z(:,idx);
@@ -25,3 +25,23 @@ t = t(:,idx);
 tri = get_face_connect(x);
 figure
 tricontour(tri,y(:),z(:),t(:),0:0.5:100,'r');
+
+if 0 % To output
+    fid = fopen('cplot_tpv5.txt','wt');
+    fprintf(fid,'# problem = TPV5\n');
+    fprintf(fid,'# author = Wenqiang Zhang\n');
+    fprintf(fid,'# date = 2023/02/13\n');
+    fprintf(fid,'# code = DRDG3D\n');
+    fprintf(fid,'# code_version = 0.0\n');
+    fprintf(fid,'# element_size = 200 m on fault, Order 4\n');
+    fprintf(fid,'# Column #1 = horizontal coordinate, distance along strike (m)\n');
+    fprintf(fid,'# Column #2 = vertical coordinate, distance down-dip (m)\n');
+    fprintf(fid,'# Column #3 = rupture time (s)\n');
+    fprintf(fid,'# The line below lists the names of the data fields:\n');
+    fprintf(fid,'j        k       t\n');
+
+    t(t<0)=1e9;
+    dat = [y(:)*1e3,z(:)*-1e3,t(:)];
+    fprintf(fid,'%14.6e %14.6e %14.6e\n',dat');
+    fclose(fid);
+end
