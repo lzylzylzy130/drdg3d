90df2f12f724d8c08ea735004f14da8eba3b2d74
Lu Zeyu <luzeyu@mail.ustc.edu.cn>
2023-07-26 15:42:16 +0800

    Change for multiple subfaults

--- examples/basin_alper/draw_rupspeed.m
+++ examples/basin_alper/draw_rupspeed.m
@@ -16,7 +16,9 @@ nx = gather_fault_var( data_dir, nproc, 'nx' );
 t = gather_fault_var( data_dir, nproc, 'ruptime' );
 fault_id = gather_fault_var1d( data_dir, nproc, 'fault_id' );
 
-idx = find(mean(nx(:,:))>0 & fault_id == 1);
+% idx = find(mean(nx(:,:))>0 & fault_id == 4);
+% idx = find(mean(nx(:,:))>0 & (fault_id == 1 | fault_id == 2));
+idx = find(mean(nx(:,:))>0 & (fault_id == 3 | fault_id == 4 | fault_id == 5));
 x = x(:,idx);
 y = y(:,idx);
 z = z(:,idx);
