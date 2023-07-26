6ff1a9b00d32a66a3f57d846b12dc020916b9c02
Lu Zeyu <luzeyu@mail.ustc.edu.cn>
2023-07-26 09:59:40 +0800

    Change of go.sh file

--- examples/tpv5/go.sh
+++ examples/tpv5/go.sh
@@ -8,4 +8,4 @@ bin_dir='../../bin'
 ${bin_dir}/exe_get_neigh mesh.nc
 mkdir -p data
 ${bin_dir}/exe_part_mesh mesh.nc
-mpirun --oversubscribe -np 4 ${bin_dir}/exe_solver 
+/opt/homebrew/Cellar/open-mpi/4.1.4_2/bin/mpirun --oversubscribe -np 4 ${bin_dir}/exe_solver 
